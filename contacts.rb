faaab8cb44d7e1c2804660bd9ae079575bf84522
Jocelyn Naarden <tzionone@gmail.com>
2022-07-12 21:45:26 -0400

    Code Along Completed 💯

--- contacts.rb
+++ contacts.rb
@@ -17,9 +17,34 @@ def contacts
   }
 end
 
+# Original Code B4 looking @ code along
 def remove_strawberry(contacts)
   # your code here!
+  # Iterate top level
+  contacts.each do |person, data|
+    if person == "Freddy Mercury"
+      data.each do |key, value|
+        if key == :favorite_ice_cream_flavors
+          value.delete("strawberry")
+        end
+      end
+    end
+  end
 end
+
+# Code from Lesson
+# def remove_strawberry(contacts)
+#   contacts.each do |person, contact_details_hash|
+#     if person == "Freddy Mercury"
+#       contact_details_hash.each do |attribute, data|
+#         if attribute == :favorite_ice_cream_flavors
+#           data.delete_if {|ice_cream| ice_cream == "strawberry"}
+#         end
+#         # binding.pry
+#       end
+#     end
+#   end
+# end
 
 # print the output to the terminal for inspection
 pp remove_strawberry(contacts)
